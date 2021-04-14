de3855d1e50587b2baa8cbe7265675bdd15121ad
brad <bradleylignoski@gmail.com>
2021-04-14 07:42:18 -0700

    Committing for merge. Can't think of any significant changes.

--- src/app/services/timer.service.ts
+++ src/app/services/timer.service.ts
@@ -5,19 +5,27 @@ import { BehaviorSubject, Observable,ReplaySubject } from '../../../node_modules
 
 class StopWatch {
 
-  public user:UserArrayEntry = null;
+  public uid:number = null;
 
   private isPaused:boolean = true;
-  private elapsedTime:number = 0;
-
+  private elapsedSeconds:number = 0;
   private worker: Worker;
+  private onMessageCallback:any = null;
 
-  constructor(user:UserArrayEntry, worker: Worker){
-    this.user = user;
+  constructor(uid:number, worker: Worker){
+    this.uid = uid;
     this.worker = worker;
+
+    //TODO: probably don't do this in constructor.
+    //...need to be able to disable this callback when a differnt user is being timed.
+    this.onMessageCallback = this.worker.onmessage = (event) => {
+      console.log(`user: ${uid} got time as: ${event.data} seconds.`);
+      console.log(event.data);
+      this.elapsedSeconds = event.data;
+    }
   }
 
-  startTimer(){
+  start(){
   }
 
   pause(){
@@ -39,7 +47,7 @@ class StopWatch {
 export class TimerService {
   elapsedSeconds = 0;
   simulatedBadTime = 0; //TODO: just for testing: delete this
-  stopWatch:StopWatch;
+  stopWatches:{[key:number]: StopWatch}; //key is the userId of the person with this stopwatch
   localTimer:any; //a setInterval instance
 
   private elapsedTime$: BehaviorSubject<number> = new BehaviorSubject<number>(null);
@@ -61,13 +69,11 @@ export class TimerService {
       this.elapsedSeconds = event.data;
     }
 
-
     //for testing
     setTimeout(()=>{
       console.log("posting STOP message !!!!!!!!!");
       this.timerWorker.postMessage("stop");
     }, 6000);
-
 
     console.log("POSTING MESSAGE TO WORKER:")
     this.timerWorker.postMessage("start");
@@ -83,6 +89,39 @@ export class TimerService {
       clearInterval(this.localTimer);
       this.localTimer = null;
     }, 1500*3 );
+  }
 
+  /*
+  * @remarks: check the stopWatches object for this user's stopwatch. If
+  * not available, create it and put it in the stopWatches object.
+  */
+  createStopwatchForUser(uid:number){
+    if(this.stopWatches[uid]){
+      return this.stopWatches[uid];
+    }else{
+      if (!this.timerWorker){
+        this.timerWorker = new Worker("./workers/timer.worker", { type: `module` } );
+      }
+      const watch = new StopWatch(uid,this.timerWorker);
+      this.stopWatches[uid] = watch;
+    }
+  }
+
+  startTimingUser(uid:number){
+    try{
+      this.stopWatches[uid].start();
+    }
+    catch(error){
+      throw(error);
+    };
+  }
+
+  stopTimingUser(uid:number){
+    try{
+      this.stopWatches[uid].stop();
+    }
+    catch(error){
+      throw(error);
+    };
   }
 }

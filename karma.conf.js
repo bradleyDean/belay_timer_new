181a49d35abff2e4e5bbf4ebcc748f84817718dc
brad <bradleylignoski@gmail.com>
2023-06-04 11:54:09 -0600

    Fix test autorun and install chrome on circleci.

--- karma.conf.js
+++ karma.conf.js
@@ -2,6 +2,7 @@
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
 module.exports = function (config) {
+  const isCi = process.env.CI === 'true'; // CI is set in .circleci/config.yaml
   config.set({
     basePath: '',
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
@@ -36,9 +37,9 @@ module.exports = function (config) {
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
-    autoWatch: true,
+    autoWatch: !isCi,
     browsers: process.env.CI ? ['ChromeHeadless'] : ['Chrome'],
-    singleRun: false,
-    restartOnFileChange: true
+    singleRun: isCi,
+    restartOnFileChange: !isCi 
   });
 };
